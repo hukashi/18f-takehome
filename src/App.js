0d064dd23f9511a7e768acfefcd597dd290279dc
Jonathan Chung <jonathan.chung@sooryen.com>
2020-07-25 19:52:37 -0400

    add search field and update with funcionality for filtering array of posts

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 
 import { Posts } from "./components/posts/posts.component";
+import { SearchBox } from "./components/search-box/search-box.component";
 
 class App extends React.Component {
   constructor() {
@@ -20,10 +21,19 @@ class App extends React.Component {
 
   render() {
     const { posts, searchField } = this.state;
+    const filteredPosts = posts.filter((post) =>
+      post.title.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
+        <SearchBox
+          placeholder="Search Posts"
+          handleChange={(e) => {
+            this.setState({ searchField: e.target.value });
+          }}
+        />
         <h1 className="title">POSTS</h1>
-        <Posts posts={posts} />
+        <Posts posts={filteredPosts} />
       </div>
     );
   }
